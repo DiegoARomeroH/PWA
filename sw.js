77a2207e709ace1c63142f71603bab4407ab5f8d
DiegoARomeroH <diegoalonsoromerohernndez@gmail.com>
2025-10-16 11:21:35 -0600

    03 Rework

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // 1. Nombre del sw y los archivos a cachear
 const CACHE_NAME = 'mi-cache';
-const BASE_PATH = 'PWA/';
+const BASE_PATH = '/PWA/';
 const urlsToCache = [
     `${BASE_PATH} index.html`,
     `${BASE_PATH} manifest.json`,
